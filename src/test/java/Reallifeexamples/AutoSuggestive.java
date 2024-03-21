7b3176fffd1f92a7c14f65cb4bb5ff09035894c1
Arunjo-se <arunjosearcj@gmail.com>
2024-03-21 17:39:58 +0530

    to testNG

--- src/test/java/Reallifeexamples/AutoSuggestive.java
+++ src/test/java/Reallifeexamples/AutoSuggestive.java
@@ -5,14 +5,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class AutoSuggestive {
 
-	public static void main(String[] args) throws InterruptedException {
+//	public static void main(String[] args) throws InterruptedException {
 
 // TODO Auto-generated method stub
 
-		WebDriver driver = new ChromeDriver();
+	WebDriver driver = new ChromeDriver();
+
+	@Test
+	public void f1() throws InterruptedException {
 
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
 
@@ -20,8 +24,16 @@ public class AutoSuggestive {
 
 		Thread.sleep(3000);
 
-		List<WebElement> options = driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+//		 List<WebElement> options =
+//		 driver.findElements(By.cssSelector("li[class='ui-menu-item'] a"));
+//		 
+			
 
+
+		List<WebElement> options = driver.findElements(By.xpath(
+				"//ul[@class=\"ui-autocomplete ui-front ui-menu ui-widget ui-widget-content ui-corner-all\"]//li"));
+
+		Thread.sleep(1000);
 		for (WebElement option : options)
 
 		{
@@ -29,14 +41,15 @@ public class AutoSuggestive {
 			if (option.getText().equalsIgnoreCase("India"))
 
 			{
+				Thread.sleep(3000);
+
 				option.click();
-
 				break;
-
 			}
-
+			
 		}
 
-	}
+		
 
+	}
 }
