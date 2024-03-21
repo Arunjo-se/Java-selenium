d6be53d8387d5e9cd7a67c64f9db17c2a8e4d1c2
Arunjo-se <arunjosearcj@gmail.com>
2024-03-22 01:06:59 +0530

    checkbox1

--- src/test/java/Reallifeexamples/CheckBox.java
+++ src/test/java/Reallifeexamples/CheckBox.java
@@ -6,19 +6,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class CheckBox {
-	
-	WebDriver driver = new ChromeDriver();
-	
-  @Test
-  public void f() {
-	  
-	  driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-	  
-	  driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_chk_friendsandfamily\"]")).click();
-	  
-	  System.out.println(driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_chk_friendsandfamily\"]")).isSelected());
-	  
-	  // isSelected is used to understand the checkbox selected or 
 
-  }
+	WebDriver driver = new ChromeDriver();
+
+	@Test
+	public void f() {
+
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+		driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_chk_friendsandfamily\"]")).click();
+
+		System.out.println(
+				driver.findElement(By.xpath("//input[@id=\"ctl00_mainContent_chk_friendsandfamily\"]")).isSelected());
+
+		// isSelected is used to understand the checkbox selected or
+
+	}
 }
